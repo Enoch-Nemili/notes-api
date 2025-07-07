5de41d39a550a0a636d6a417100cb5d050c8e28b
Enoch Nemili <enoch.das@gmail.com>
2025-07-07 20:06:08 +0530

    Fix: Import note.js correctly (case-sensitive)

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const Note = require('./models/Note');
+const Note = require('./models/note');
 
 // 2. Create Express app
 const app = express();
